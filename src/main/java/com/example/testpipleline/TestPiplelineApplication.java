14338ca625dc55d50c51066ed8975f356b1e84b5
wangxulong <duanxian0402@126.com>
2020-03-12 11:37:22 +0800

    init commit

---
+++ src/main/java/com/example/testpipleline/TestPiplelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpipleline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPiplelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPiplelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpipleline/TestPiplelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpipleline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPiplelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
